afafdf48ae47adf71388421cd2446b882ad2635c
Samuel Hautcoeur <samuel.hautcoeur@ep4.com>
2018-07-17 11:33:49 +0000

    Clean cache locks on periodicBackup

--- imagescripts/create_jobber.sh
+++ imagescripts/create_jobber.sh
@@ -13,6 +13,8 @@ cat > ${JOBBER_SCRIPT_DIR}/periodicBackup <<_EOF_
 
 set -o errexit
 
+sh ${VOLUMERIZE_SCRIPT_DIR}/cleanCacheLocks
+
 if [ "${VOLUMERIZE_MYSQL_BACKUPS}" = 'true' ]; then
   mysqldump --single-transaction --routines --events --triggers --add-drop-table --extended-insert -u ${VOLUMERIZE_MYSQL_USER} -h ${VOLUMERIZE_MYSQL_HOST} -p${VOLUMERIZE_MYSQL_PASSWORD} --all-databases | gzip -9 > ${VOLUMERIZE_SOURCE}/mysql/db_\$(date +"%H:%M_%d-%m-%Y").sql.gz
 fi
